5ab054b4ab77637a5acd81e775969d8692ca5eb3
Holger Harms <holhar@posteo.de>
2022-01-12 16:31:36 +0100

    Add Pipe examples for formatting data

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-course',
+  selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  title = "The complete Angular course";
+  rating = 4.9745;
+  students = 30213;
+  price = 190.95;
+  releaseDate = new Date(2016, 3, 1);
 
   constructor() { }
 
