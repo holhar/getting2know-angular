174e2fa9bb022ef8228be31e9f031da64f7ba3ac
Holger Harms <holhar@posteo.de>
2022-01-12 16:38:39 +0100

    Implement and apply custom SummaryPipe

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
   title = "The complete Angular course";
+  text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
   rating = 4.9745;
   students = 30213;
   price = 190.95;
