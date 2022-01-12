a9dc6b8956920972411e5b6e9547863e8170db53
Holger Harms <holhar@posteo.de>
2022-01-12 15:46:16 +0100

    Property binding example: add image in courses template

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,6 +6,7 @@ import { CoursesService } from './courses.service';
     selector: 'courses',
     template: `
         <h2>{{ title }}</h2>
+        <img [src]="imageUrl" />
         <ul>
             <li *ngFor="let course of courses">
                 {{ course }}
@@ -15,6 +16,7 @@ import { CoursesService } from './courses.service';
 })
 export class CoursesComponent {
     title = "List of courses";
+    imageUrl = "https://picsum.photos/400/200"
     courses;
 
     constructor(service: CoursesService) {
