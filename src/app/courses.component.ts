0d4dc0f9afc43a7e869ed8ca4b7b5b3ebd34acf9
Holger Harms <holhar@posteo.de>
2022-01-12 15:13:46 +0100

    Templating: using *ngFor Directive

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 // Applying 'Component' decorator function
 @Component({
     selector: 'courses',
-    template: '<h2>{{ "Title: " + getTitle() }}</h2>' 
+    template: `
+        <h2>{{ "Title: " + getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
     title = "List of courses";
+    courses = ["course1", "course2", "course3"];
 
     getTitle() {
         return this.title;
