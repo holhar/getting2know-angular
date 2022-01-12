d327feb019a544e17d9cfaa38af5f189ddf596ab
Holger Harms <holhar@posteo.de>
2022-01-12 12:03:10 +0100

    Implement first Component

---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+// Applying 'Component' decorator function
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>' 
+})
+export class CoursesComponent {
+}
