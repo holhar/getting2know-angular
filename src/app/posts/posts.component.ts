e1914b0b7e367c8f207f33c843491ce53dd5964c
Holger Harms <holhar@posteo.de>
2022-01-13 17:01:08 +0100

    Implement example post request with HttpClient

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,13 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
+  private url: string = 'http://jsonplaceholder.typicode.com/posts';
+
   posts: any[] = [];
 
-  constructor(http: HttpClient) {
-    http.get('http://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient) {
+    http.get(this.url)
       .subscribe(data => {
         this.posts = JSON.parse(JSON.stringify(data));
         console.log(this.posts);
       });
+  }
+
+  createPost(input: HTMLInputElement) {
+      let post: any = { title: input.value };
+      input.value = '';
+
+      this.http.post(this.url, JSON.stringify(post))
+        .subscribe(data => {
+          let response = JSON.parse(JSON.stringify(data));
+          post['id'] = response.id;
+          this.posts.splice(0, 0, post);
+          console.log(response);
+        });
   }
 }
