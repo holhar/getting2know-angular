4699ad8a7e25dfc068b9a3ecb637a74f7ec17126
Holger Harms <holhar@posteo.de>
2022-01-13 15:59:13 +0100

    Implement NewCourseForm component to illustrate FormArray usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CourseFormComponent } from './course-form/course-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
     InputFormatDirective,
     ZippyComponent,
     ContactFormComponent,
-    CourseFormComponent
+    CourseFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
