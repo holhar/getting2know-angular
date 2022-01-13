8def6880418526c4dfb7e06122df8267b56626e9
Holger Harms <holhar@posteo.de>
2022-01-13 15:42:43 +0100

    Apply nested formGroups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,12 +9,14 @@ import { UsernameValidators } from './username.validators';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotCaontainSpace
-    ], UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotCaontainSpace
+      ], UsernameValidators.shouldBeUnique),
+      password: new FormControl('', Validators.required)
+    })
   });
 
   login() {
@@ -28,6 +30,6 @@ export class SignupFormComponent {
   }
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
