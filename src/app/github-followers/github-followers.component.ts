56419e26603e350f056162de5adf06be1e388583
Holger Harms <holhar@posteo.de>
2022-01-14 16:28:51 +0100

    Applying query params in links and subscripe to multiple observables

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'github-followers',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubFollowersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    combineLatest([
+      this.route.paramMap, 
+      this.route.queryParamMap
+    ])
+    .subscribe(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+
+      // this.service.getAll({ id: id, page: page });
+    })
   }
 
 }
