7333ea75c112324fe250ec3baff4afde55d77117
Holger Harms <holhar@posteo.de>
2022-01-14 16:03:36 +0100

    'Why route parameters are observable...'

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,10 +11,16 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log("github-profile component initialized");
+
+    //let id = this.route.snapshot.paramMap.get('id');
+    //console.log('id retrieved directly by snapshot: ' + id);
+
+    // Retrieve id via observable without destroying the github profile component
     this.route.paramMap
       .subscribe(params => {
         let id = params.get('id');
-        console.log(id);
+        console.log('id retrieved by observable: ' + id);
       });
   }
 
